98429fad718ece133960d2c4d378b7fa4b99ff42
Ashfaq9999 <ashfaqmd007@gmail.com>
2025-03-05 11:27:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region="ap-south-1"
 }
 
-resources "aws_instance" "ec2" {
+resource "aws_instance" "ec2" {
 ami="ami-0ddfba243cbee3768"
 instance_type= "t2.micro"
 key_name= "keypair"
