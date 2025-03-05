07ea9dec54798d286ce44de58b340a419477a2fd
Ashfaq9999 <ashfaqmd007@gmail.com>
2025-03-05 11:31:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region="ap-south-1"
 }
 
 resource "aws_instance" "ec2" {
-ami="ami-0ddfba243cbee3768"
-instance_type= "t2.micro"
-key_name= "keypair"
+  ami= "ami-0ddfba243cbee3768"
+  instance_type= "t2.micro"
+  key_name= "keypair"
 
 }
